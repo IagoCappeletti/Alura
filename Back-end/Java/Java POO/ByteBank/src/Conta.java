9bdbe09982fd060070d6cdf68a5aba413e463773
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-05-24 01:36:53 -0300

    Exercicio inicial sobre herença e polimorfismo

--- Back-end/Java/Java POO/ByteBank/src/Conta.java
+++ Back-end/Java/Java POO/ByteBank/src/Conta.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Conta {
+
+
+public class Conta {
+    private Double saldo;
+    private Integer agencia;
+    private Integer numero;
+    private Cliente titular;
+    private static Integer totalContas = 0;
+
+    public Conta(Integer agencia, Integer numero) {
+        Conta.totalContas++;
+        System.out.println("O total de contas é: " + totalContas);
+
+        this.agencia = agencia;
+        this.numero = numero;
+        System.out.println("NúmeroDaConta: " + this.numero);
+        System.out.println("Agencia: " + this.agencia);
+    }
+
+    public void depositar(Double valor){
+        this.saldo += valor;
+    }
+    public boolean saca(double valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean transfere(double valor, Conta destino){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public Integer getAgencia() {
+        return agencia;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public static Integer getTotalContas() {
+        return totalContas;
+    }
 }
--- Back-end/Java/Java POO/ByteBank/src/Program.java
+++ Back-end/Java/Java POO/ByteBank/src/Program.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Program {
+public class Program {
+    public static void main(String[] args) {
+
+        Conta conta = new Conta(1010,1337);
+        Conta conta1 = new Conta(1010,2912);
+        Conta conta2 = new Conta(1010,888838);
+
+        System.out.println("Total de contas: " + Conta.getTotalContas());
+
+    }
 }
