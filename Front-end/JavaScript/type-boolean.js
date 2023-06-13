60047206f301635c94910b76ef08b752e74fd95b
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-06-13 01:38:12 -0300

    exemplos sobre boolean

---
+++ Front-end/JavaScript/type-boolean.js
@@ -0,0 +1,16 @@
+let n1 = 10;
+let n2 = 10;
+let txt1 = "alura";
+let txt2 = "Alura";
+
+if (n1 == n2) {
+    console.log("O número são iguais!");
+} else {
+    console.log("Os números são diferentes!");
+}
+
+if (txt1 == txt2) {
+    console.log("Os textos são iguais!");
+} else {
+    console.log("Os textos são diferentes!");
+}
