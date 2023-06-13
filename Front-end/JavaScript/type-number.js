56e6565c11017de737fd99e7adf72778f8c2a20c
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-06-13 01:16:16 -0300

    Números em JS

--- Front-end/JavaScript/type-number.js
+++ Front-end/JavaScript/type-number.js
@@ -1,9 +1,13 @@
-//tipo number
-
 const meuNumero = 3;
 
 const primeiroNumero = 1;
 const segundoNumero = 2;
 
 const operacaoAdicao = primeiroNumero + segundoNumero;
-console.log(operacaoAdicao);
+console.log(operacaoAdicao)
+
+//pontos flutuante
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteSemZero = 1.5;
+const novaOperacao = numeroPontoFlutuante / numeroPontoFlutuanteSemZero;
+console.log(Math.round(novaOperacao));
