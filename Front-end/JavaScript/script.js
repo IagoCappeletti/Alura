b624ec75706df208405041597ab3b118f07a2e5a
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-06-13 02:06:21 -0300

    três formas de escrever variáveis

---
+++ Front-end/JavaScript/script.js
@@ -0,0 +1,33 @@
+//var 
+//var altura = 5;
+//var comprimento = 7;
+//area = altura * comprimento;
+//console.log(area);
+//var area;
+
+//let
+// let fomra = "retangulo";
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+// if (fomra === "retangulo") {
+//     area = altura * comprimento;
+//     console.log(area);
+// } else {
+//     area = (altura * comprimento) / 2;
+//     console.log(area);
+// }
+
+//const
+const fomra = "quadrado";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (fomra === "quadrado") {
+        area = altura * comprimento;
+    } else {
+        area = (altura * comprimento) / 2;
+    }
+
+    console.log(area)
