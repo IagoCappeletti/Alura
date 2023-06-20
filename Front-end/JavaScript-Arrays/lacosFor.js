b48175f3ee36439055215a50add133f1516b4fbf
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-06-20 17:38:56 -0300

    Exemplos de Laços for

---
+++ Front-end/JavaScript-Arrays/lacosFor.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+for (let j = 0; j < numeros.length; j++) {
+   console.log(numeros[j]);
+    
+}
+//Só brincando com o código
+// for (let i = 0; i <= 100; i+=10){
+//     console.log(i);
+// }
