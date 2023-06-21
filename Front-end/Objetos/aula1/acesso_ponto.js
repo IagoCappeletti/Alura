2802fd4f15170f61e45c2735a4a7aa987f397beb
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-06-21 13:37:16 -0300

    Acessando informação do Objeto com .

---
+++ Front-end/Objetos/aula1/acesso_ponto.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Iago",
+    idade: 22,
+    cpf: "1091111112",
+    email: "Iago.cliente@gmail.com"
+}
+
+console.log(`O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos.`);
+console.log(`Os 3 primeiros números do CPF são ${cliente.cpf.substring(0, 3)}`)
