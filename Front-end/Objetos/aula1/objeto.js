54b32c4620569e36aa842a0d9465a50e367d1535
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-06-21 13:36:41 -0300

    Aprendendo a estrutura de um Objeto

---
+++ Front-end/Objetos/aula1/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = [111111111, 22222222, 333333333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "1111222333"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+  idade: 32,
+  nome: "Jose",
+};
