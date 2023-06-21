1cdb3d4dafdb2fb424536e455a32dd37c6142b8e
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-06-21 17:29:14 -0300

    Colocando um Objeto dentro de outro Objeto

---
+++ Front-end/Objetos/aula2/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joaocliente@firmal.com",
+    telefone: ["5198746364", "2126336463"],
+};
+
+ cliente.endereco = {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "Ap 999",
+ };
+
+ console.log(cliente)
