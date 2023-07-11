c26909245e6f4e5647e15691e6bc5f16b0b04dc3
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-07-11 16:33:51 -0300

    Exercicios para consolidar conhecimentos ensinados durante o curso

---
+++ Back-end/Java/Java POO/BuscaCep/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ConsultaCep consultaCep = new ConsultaCep();
+        System.out.print("Digite um Nº de CEP: ");
+        var cep = sc.nextLine();
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo geradorDeArquivo = new GeradorDeArquivo();
+            geradorDeArquivo.geraJson(novoEndereco);
+        }catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando aplicação! ");
+
+        }
+    }
+}
