a6be428de43458a13cbf2b7379e45b9f1f697932
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-07-25 14:59:59 -0300

    Mapeando JPA

--- Back-end/Java/Java Spring/api/src/main/java/med/voll/api/endereco/DadosEnderecos.java
+++ Back-end/Java/Java Spring/api/src/main/java/med/voll/api/endereco/DadosEnderecos.java
@@ -1,4 +1,12 @@
 package med.voll.api.endereco;
 
-public record DadosEnderecos(String logradouro, String bairro, String cep, String cidade,String uf, String complemento, String numero) {
+public record DadosEnderecos(
+        String logradouro,
+        String bairro,
+        String cep,
+        String cidade,
+        String uf,
+        String complemento,
+        String numero
+) {
 }
