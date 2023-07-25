6475ea64e80469785bec6b51c234cf71a51742b4
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-07-25 15:21:55 -0300

    criado Spring Data e interface Repository

--- Back-end/Java/Java Spring/api/src/main/java/med/voll/api/endereco/Endereco.java
+++ Back-end/Java/Java Spring/api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -17,6 +17,15 @@ public class Endereco {
     private String numero;
     private String complemento;
     private String cidade;
-    private String UF;
+    private String uf;
 
+    public Endereco(DadosEnderecos dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+    }
 }
