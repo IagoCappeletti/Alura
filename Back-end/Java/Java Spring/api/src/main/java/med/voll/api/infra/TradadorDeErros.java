9adf331c5e88d149fa61da58053e11e71e6e90b5
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-08-09 15:48:25 -0300

    Tratadndo o erro 400 e devolvendo um JSON simplificado com o erro

--- Back-end/Java/Java Spring/api/src/main/java/med/voll/api/infra/TradadorDeErros.java
+++ Back-end/Java/Java Spring/api/src/main/java/med/voll/api/infra/TradadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +15,16 @@ public class TradadorDeErros {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    //Exceção especifica quando acontece algum erro no Bean Validation
+    public ResponseEntity TratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacaoDTO::new).toList());
+    }
 
-
+    public record DadosErroValidacaoDTO(String campo, String mensagem) {
+        public DadosErroValidacaoDTO(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
 }
