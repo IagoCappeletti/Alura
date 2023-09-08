24f05efa92ea0e28429fd0721fce6e8a65947d03
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-09-08 11:14:24 -0300

    Ajustes na geração do token

--- Back-end/Java/Java Spring/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ Back-end/Java/Java Spring/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.Usuario;
 import med.voll.api.domain.usuario.dto.DadosAutenticacaoDTO;
 import med.voll.api.infra.security.TokenService;
+import med.voll.api.infra.security.dto.DadosTokenJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,9 +25,11 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var autenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(autenticationToken);
 
-        return ResponseEntity.ok( tokenService.gerarToken((Usuario) authentication.getPrincipal()));
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
