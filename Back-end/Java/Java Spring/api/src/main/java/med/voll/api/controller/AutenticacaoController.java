fc51773be5613d9793c0c266536c12c70c2f826f
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-09-08 10:28:12 -0300

    Gerando tokens JWT

--- Back-end/Java/Java Spring/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ Back-end/Java/Java Spring/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -1,7 +1,9 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.Usuario;
 import med.voll.api.domain.usuario.dto.DadosAutenticacaoDTO;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,12 +19,14 @@ public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok( tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
