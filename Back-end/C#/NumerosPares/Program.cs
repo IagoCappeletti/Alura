f0f8d8987384e6534211a3c0f41557d1222bc448
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-11-04 09:23:35 -0300

    Criado projeto que verifica se os números são pares

---
+++ Back-end/C#/NumerosPares/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach (var numero in numeros) {
+    if (numero % 2 == 0) {
+        Console.WriteLine($"O número {numero} é par");
+    } else {
+        Console.WriteLine($"O número {numero} é impar");
+    }
+}
